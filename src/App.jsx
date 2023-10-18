74c4ead99f29ff26ac8faafb7a9f75a76a1748a1
vorio <voriodev@gmail.com>
2023-10-18 22:24:22 +0200

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import ThankYou from './components/ThankYou'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/interactive-rating-component/">
       <main className="grid place-content-center padding-x-24 w-full h-[100vh] bg-very-dark-blue px-[24px]">
-				<Routes>
-					<Route index element={<Rating />}/>
-					<Route path="/thank-you/:rating" element={<ThankYou />}/>
-				</Routes>
-			</main>
+        <Routes>
+          <Route index element={<Rating />} />
+          <Route path="/thank-you/:rating" element={<ThankYou />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
